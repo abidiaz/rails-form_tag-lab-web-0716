b99f45d82fdd4dcb0527041af5f8e01e413286a6
Abigail Diaz <github email address>
2016-08-18 19:12:55 -0400

    Completed Rails Form Tag Lab

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,4 +8,23 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
   end
 
+  def new
+
+  end
+
+  def create
+    @student = Student.create(first_name: params[:first_name], last_name: params[:last_name])
+    redirect_to students_path
+    # student = Student.create(first_name: params[:first_name], last_name: params[:last_name])
+    # redirect_to student_path(@student)
+  end
+  
+
 end
+
+
+#      Prefix Verb URI Pattern             Controller#Action
+#    students GET  /students(.:format)     students#index
+#             POST /students(.:format)     students#create
+# new_student GET  /students/new(.:format) students#new
+#     student GET  /student/:id(.:format)  students#show
